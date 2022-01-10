058ad795b635a4020456508cfcd162f79f5dde22
pdomokos <domokospeter@gmail.com>
2022-01-10 19:58:31 +0100

    Add project files

---
+++ src/test/java/com/example/SensorService/TempSensorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SensorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempSensorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
